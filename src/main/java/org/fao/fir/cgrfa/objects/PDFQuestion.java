9d566346384cf92f734d3b3940d9537b985b0eb5
Enrico Anello <enrico.anello@fao.org>
2015-05-11 10:33:59 +0200

    Reference changed from absolute to relative

--- src/main/java/org/fao/fir/cgrfa/objects/PDFQuestion.java
+++ src/main/java/org/fao/fir/cgrfa/objects/PDFQuestion.java
@@ -5,10 +5,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import org.fao.fir.cgrfa.pdf.parser.objects.PDFField;
+
 @AllArgsConstructor
 public @Data class PDFQuestion {
-	org.fao.fir.cgrfa.pdf.parser.objects.PDFField singleQuestion;
-	List<org.fao.fir.cgrfa.pdf.parser.objects.PDFField> multipleQuestion;
+	PDFField singleQuestion;
+	List<PDFField> multipleQuestion;
 	PDFTable table;
 	int id;
 }
